fb497d00301f699eea59b30e4a7f2d12270bdfa5
Ryan Carlson <ryan.austin.carlson@gmail.com>
2015-03-08 22:39:26 -0400

    added a couple extras to db inspector

--- db_inspector.py
+++ db_inspector.py
@@ -1,11 +1,16 @@
 
+import sys
 import pymongo
 from pprint import pprint
 
 client = pymongo.MongoClient('mongodb://localhost:27017/db')
 db = client.get_default_database()
 
-collections = ['people', 'events', 'reservations', 'rideshares', 'requests']
+collections = ['people',
+               'events',
+               'reservations',
+               'rideshares',
+               'requests']
 
 
 def find_stuff(stuff):
@@ -17,11 +22,17 @@ def find_stuff(stuff):
 
 for collection in collections:
     find_stuff(collection)
-    # db[collection].remove()
 
+    if len(sys.argv) == 2:
+        if sys.argv[1] == 'remove':
+            db[collection].remove()
+
+# db.rideshares.remove()
 
 # people = [x for x in db.people.find()]
 # events = [x for x in db.events.find()]
 # reservations = [x for x in db.reservations.find()]
 # rideshares = [x for x in db.rideshares.find()]
 # requests = [x for x in db.requests.find()]
+
+print([x for x in db.people.find({'email': 'ryan.austin.carlson@gmail.com'})])
